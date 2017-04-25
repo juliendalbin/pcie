7f29255af0364615a92de87a74743fc11cc8b748
Julien <julien.dalbin@gmail.com>
2017-04-25 16:57:33 +0200

    success deprecated, then instead

--- app/js/factory/json/localFactory.js
+++ app/js/factory/json/localFactory.js
@@ -2,7 +2,7 @@ PCIE.factory("LocalFactory", function ($http, $q) {
     return {
         menu: function () {
             var deffered = $q.defer();
-            $http.get('data/menu.json').success(function (response) {
+            $http.get('data/menu.json').then(function (response) {
                 deffered.resolve(response.menu);
             })
             return deffered.promise;

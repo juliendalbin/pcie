476f3bed7d8ef4e719463d1d85f808fbc84eb0bb
Julien <julien.dalbin@gmail.com>
2017-04-26 09:19:29 +0200

    console log success

--- app/js/factory/json/localFactory.js
+++ app/js/factory/json/localFactory.js
@@ -4,6 +4,7 @@ PCIE.factory("LocalFactory", function ($http, $q) {
             var deffered = $q.defer();
             $http.get('data/menu.json').then(function (success){
             	deffered.resolve(success.menu);
+            	console.log("success",success);
   			},function (error){
   				deferred.reject(error);
   				console.log("error",error);

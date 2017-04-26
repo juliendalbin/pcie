6554a6e749b3df3ac03f2b3fe9dd1c4e14d06615
Julien <julien.dalbin@gmail.com>
2017-04-26 11:36:36 +0200

    remove setBaseURl

--- app/js/factory/rest/restService.js
+++ app/js/factory/rest/restService.js
@@ -1,9 +1,6 @@
 PCIE.factory("RestService", [ "Restangular", "$rootScope", function(Restangular, $rootScope) {
 	return {
 		create : function(service) {
-
-	        Restangular.setBaseUrl('');;
-
 			return Restangular.all(service);
 		}, 
 	}

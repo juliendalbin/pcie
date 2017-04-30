da3d8fade397f317755f36aedcc940f53b415f71
Julien <julien.dalbin@gmail.com>
2017-04-30 15:38:05 +0200

    factories with

--- app/js/factory/rest/authFactory.js
+++ app/js/factory/rest/authFactory.js
@@ -3,7 +3,7 @@ PCIE.factory("authFactory", function($http, $q) {
         login: function (user) {
             var deferred = $q.defer();
             $http.post("/auth",user).then(function (success){
-                deffered.resolve(success.data);
+                deferred.resolve(success.data);
             },function (error){
                 deferred.reject(error);
             });
